f9528070f055fc3a83940cecb7a0acd0354cb240
Hajk <tunczorx@gmail.com>
2018-03-12 14:05:02 +0100

    Learning methods and classes

--- TestingMethodsAndClasses/Form1.cs
+++ TestingMethodsAndClasses/Form1.cs
@@ -16,5 +16,21 @@ namespace TestingMethodsAndClasses
         {
             InitializeComponent();
         }
+        //Minimalize & Maximized buttons disabled
+        //After clicking button, application will display message which user introduced as many times as user want 
+        //then will count length of message including spacebars but without enters
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numberOfTimesLabel.Value == 0 || numberOfTimesLabel.Value > 10) //I could use min and max value avaiable for NumericUpDown object , but wanted to display message what numbers are allowed here
+            {
+                MessageBox.Show("Wprowadź liczbę całkowitą od 1 do 10");
+            }
+            else
+            { 
+            int x = Talker.BlaBlaBla(textLabel.Text, (int)numberOfTimesLabel.Value);
+            MessageBox.Show("Dlugość wiadomości to " + x);
+            }
+        }
     }
 }
---
+++ TestingMethodsAndClasses/Talker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingMethodsAndClasses
+{
+    class Talker
+        //Simple class, with static method BlaBlaBla, with string and int parameter.
+
+        
+    {
+        public static int BlaBlaBla (string thingsToSay, int numberOfTimes) // 
+        {
+            string finalString = ""; //Creating empty string variable
+            int decreaser = 0; //My variable used to cancel the "\ n" effect. So finalString.Lenght gives properly value which not include enter as character
+            for (int count = 1; count <= numberOfTimes; count ++) // Loop which repeat words as many times as user wants (max value 10)
+            {
+                finalString = finalString + thingsToSay + "\n"; 
+                decreaser++;
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length - decreaser;
+
+         
+        }
+    }
+}
